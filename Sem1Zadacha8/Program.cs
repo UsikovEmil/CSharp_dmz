771008bdcf6ef732719a53ba2d5eaaea96d8db10
Emil <usikov.emil@yandex.ru>
2022-12-18 23:33:21 +0500

    Задача 8 Выполнена

---
+++ Sem1Zadacha8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int i = 0;
+Console.WriteLine("(Показывает все чётные числа от 1 до N?)");
+
+Console.Write("Введите число N:= ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {   
+        Console.Write(i);
+        if (i < a-1)
+        {
+            Console.Write(",");
+        }
+    }
+    i++;
+}
