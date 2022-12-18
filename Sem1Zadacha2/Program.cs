6258230299caac1fd0e42e88068def80ca322e5c
Emil <usikov.emil@yandex.ru>
2022-12-18 22:29:54 +0500

    Задача 2 Готова

--- Sem1Zadacha2/Program.cs
+++ Sem1Zadacha2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("(Какое число большее, а какое меньшее?)");
+
+Console.Write("Введите первое число:= ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе  число:= ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Первое число больше второго на: {a-b}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Второе число больше первого на: {b-a}");
+}
+else
+{
+    Console.WriteLine("Извение но эти два числа равны"); 
+}
+
