c1c0c0840c8531a15bff52190173d4d1cc082832
Emil <usikov.emil@yandex.ru>
2022-12-18 22:56:05 +0500

    Задача 4 Выполнена

--- Sem1Zadacha4/Program.cs
+++ Sem1Zadacha4/Program.cs
@@ -1,20 +1,29 @@
-﻿Console.WriteLine("(Какое число большее, а какое меньшее?)");
+﻿Console.Clear();
+
+Console.WriteLine("(Какое число большее, а какое меньшее?)");
 
 Console.Write("Введите первое число:= ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе  число:= ");
+Console.Write("Введите второе число:= ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-if (a > b)
+Console.Write("Введите третье число:= ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
 {
-    Console.WriteLine($"Максимальное число = ({a}), оно число больше второго на: {a-b}");
+    Console.WriteLine($"Максимальное число = ({a}), оно больше второго на: {a-b} и третьего на: {a-c}");
 }
-else if (a < b)
+else if (b > a && b > c)
 {
-    Console.WriteLine($"Максимальное число = ({b}), оно больше первого на: {b-a}");
+    Console.WriteLine($"Максимальное число = ({b}), оно больше первого на: {b-a} и третьего на: {b-c}");
+}
+else if (c > a && c > b)
+{
+    Console.WriteLine($"Максимальное число = ({c}), оно больше первого на: {c-a} и второго на: {c-b}");
 }
 else
 {
-    Console.WriteLine("Извените но эти два числа равны"); 
+    Console.WriteLine("Извените но эти три числа равны"); 
 }
