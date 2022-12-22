ed37b17eb2715e8af822dee8e693f0b1e2afa84a
Emil <usikov.emil@yandex.ru>
2022-12-23 00:26:54 +0500

    Задание 13.1

---
+++ Sem2Zadacha13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+int n = new Random().Next(10, 2000);
+
+int s = n.ToString().Length;
+
+Console.WriteLine($"Случайное число: {n}");
+
+if (s >= 3)
+{
+    int x = n % 10;
+    Console.WriteLine($"Третья цифра = {x}");
+    Console.WriteLine(s);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
+
+
+
