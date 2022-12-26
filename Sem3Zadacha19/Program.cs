62378bca3ef9f8104d39ded45b134fd6cea5bc1e
Emil <usikov.emil@yandex.ru>
2022-12-26 14:49:35 +0500

    Задача №19 Готоваы

--- Sem3Zadacha19/Program.cs
+++ Sem3Zadacha19/Program.cs
@@ -1,32 +1,37 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 Console.Clear();
 
-//int a = Convert.ToInt32(Console.ReadLine());
-int a = 12821;
+int a = Convert.ToInt32(Console.ReadLine());
+//int a = 12821;
 int s = a.ToString().Length;
 int[] mas = new int[s];
 
-//Console.WriteLine(s);
-//int i = 0;
-//while (i <= s)
+if (s != 5)
+{
+    Console.WriteLine("Число не 5-ти значное");
+    return;
+}
+
+Console.Write($"Число {a} ({s} - значное)");
+
 for (int i = 0; i < s; i++)
 {
     mas[i] = a % 10;
     a = a / 10;
+    // Console.WriteLine($"m=[{mas[i]}]");
 
-    Console.WriteLine($"m={mas[i]}");
-    //i++;
 }
-
-/* for (int i = 0; i <= s; i++)
+int x = 0;
+for (int i = 0; i < s / 2; i++)
 {
-    mas[i] = a % 10;
-    a = a / 10;
-
-    Console.WriteLine($"m={mas[i]}");
-    //i++;
+    x = x + mas[i] - mas[s - 1 - i];
 }
-
- */
-
-
+//Console.WriteLine($"x={x}");
+if (x == 0)
+{
+    Console.Write(" и являеться палиндромом");
+}
+else
+{
+    Console.Write(" и не являеться палиндромом");
+}
