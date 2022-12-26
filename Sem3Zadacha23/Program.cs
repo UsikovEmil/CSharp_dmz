bf62a1893eaeebaa0ba43aad5868823dc704ca99
Emil <usikov.emil@yandex.ru>
2022-12-26 15:38:35 +0500

    Задача №23 Готоваа

---
+++ Sem3Zadacha23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+
+Console.Write("Введите число N = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int s = a.ToString().Length;
+
+Console.Write($"Кубы чисел от 1 до {a} = (");
+
+for (int i = 1; i <= a; i++)
+{
+    double x = Math.Pow(i, 3);
+    
+    Console.Write(x);
+    if (i < a)
+    {
+        Console.Write(",");
+    }
+    else
+    {
+        Console.Write(")");
+    }
+}
+
