78dba7a18288ccad3663e25e35bfdfa013310811
Emil <usikov.emil@yandex.ru>
2022-12-26 14:19:00 +0500

    Вариант с массивами

--- Sem3Zadacha19/Program.cs
+++ Sem3Zadacha19/Program.cs
@@ -1,4 +1,32 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 Console.Clear();
 
-Console.WriteLine("Hello, World!");
+//int a = Convert.ToInt32(Console.ReadLine());
+int a = 12821;
+int s = a.ToString().Length;
+int[] mas = new int[s];
+
+//Console.WriteLine(s);
+//int i = 0;
+//while (i <= s)
+for (int i = 0; i < s; i++)
+{
+    mas[i] = a % 10;
+    a = a / 10;
+
+    Console.WriteLine($"m={mas[i]}");
+    //i++;
+}
+
+/* for (int i = 0; i <= s; i++)
+{
+    mas[i] = a % 10;
+    a = a / 10;
+
+    Console.WriteLine($"m={mas[i]}");
+    //i++;
+}
+
+ */
+
+
