171d1d299465abefc69fd511b3f260cc7e18eb86
Emil <usikov.emil@yandex.ru>
2023-01-15 15:47:26 +0500

    Задание 27 Начало

---
+++ Sem4Zadacha29/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+int Rnum(string m)
+{
+    Console.WriteLine(m);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int mass= Rnum("Введите числа");
