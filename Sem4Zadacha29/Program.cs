1882613908d4fba3bb50d2c9c3db06d8c16f9b7c
Emil <usikov.emil@yandex.ru>
2023-01-15 19:06:01 +0500

    Задание 29 Вариант 2 Готов 3

--- Sem4Zadacha29/Program.cs
+++ Sem4Zadacha29/Program.cs
@@ -2,34 +2,16 @@
 
 Console.Clear();
 
-/* int[] Cmas(string arr)
-{
-    int len = arr.Length;
-    int[] m = new int[len];
-    int s = Convert.ToInt32(arr);
-    for (int i = 0; i < len; i++)
-    {
-
-        m[len - 1 - i] = s % 10;
-        s /= 10; // s=s/10
-    }
-    return m;
-}
-
-Console.WriteLine("Введите число");
-int[] mass2 = Cmas(Console.ReadLine());
-Console.WriteLine(string.Join(",", mass2)); */
-
 int[] Cmas(int arr)
 {
     int[] m = new int[arr];
     for (int i = 0; i < arr; i++)
     {
         Console.Write($"Число {i + 1} = ");
-        string a = Console.ReadLine();
+       /*  string a = Console.ReadLine();
         if (a == "") { a = "0"; }
-             
-        m[i] = Convert.ToInt32(a);
+        m[i] = Convert.ToInt32(a);  */
+        m[i] = Convert.ToInt32(Console.ReadLine());
     }
     return m;
 }
