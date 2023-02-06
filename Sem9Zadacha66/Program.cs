fa2679d9706f06f8fb9d2694008f4fe960d67678
Emil <usikov.emil@yandex.ru>
2023-02-06 19:02:42 +0500

    Готовв

--- Sem9Zadacha66/Program.cs
+++ Sem9Zadacha66/Program.cs
@@ -2,4 +2,24 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+Console.Write("Введите число от M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число до N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+void PrintBinaryViev(int m, int n, int s)
+{
+    if (m > n)
+    {
+        Console.Write($"Cумму натуральных элементов = {s} ");
+        return;
+    }
+    PrintBinaryViev(m + 1, n, s + m);
+
+}
+
+PrintBinaryViev(M, N, 0);
