8672ab1698ed19b1575645d8f91c6c2812b1fc12
Emil <usikov.emil@yandex.ru>
2023-02-06 19:03:04 +0500

    Не Готовв

--- Sem9Zadacha68/Program.cs
+++ Sem9Zadacha68/Program.cs
@@ -1,4 +1,25 @@
 ﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
 // m = 2, n = 3 -> A(m,n) = 9
-Console.WriteLine("Hello, World!");
+
+Console.Clear();
+// Console.Write("Введите число от M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число до N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+int M = 2;
+int N = 3;
+
+void PrintBinaryViev(int m, int n, int s)
+{
+    if (m > n)
+    {
+        Console.Write($"Cумму натуральных элементов = {s} ");
+        return;
+    }
+    PrintBinaryViev(m + 1, n, s + m);
+
+}
+
+PrintBinaryViev(M, N, 0);
