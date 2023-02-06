7d209d1d0d265b265b8028de2128b220d00bd149
Emil <usikov.emil@yandex.ru>
2023-02-06 21:22:48 +0500

    Готовв

--- Sem9Zadacha68/Program.cs
+++ Sem9Zadacha68/Program.cs
@@ -1,25 +1,22 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
 
 // m = 2, n = 3 -> A(m,n) = 9
 
 Console.Clear();
-// Console.Write("Введите число от M: ");
-// int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите положительное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write("Введите число до N: ");
-// int N = Convert.ToInt32(Console.ReadLine());
-int M = 2;
-int N = 3;
+Console.Write("Введите положительное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
-void PrintBinaryViev(int m, int n, int s)
+int PrintAkker(int m, int n)
 {
-    if (m > n)
-    {
-        Console.Write($"Cумму натуральных элементов = {s} ");
-        return;
-    }
-    PrintBinaryViev(m + 1, n, s + m);
+
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return PrintAkker(m - 1, 1);
+    else if (m > 0 && n > 0) return PrintAkker(m - 1, PrintAkker(m, n - 1));
+    return n + 1;
 
 }
-
-PrintBinaryViev(M, N, 0);
+Console.Write($"Значение функции Аккермана = {PrintAkker(M, N)} ");
